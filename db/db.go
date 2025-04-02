6ccb3e757fbf1ba770053d652659d4035501ade5
Deepak kumar yadav <V.Deepak.Yadav@myoxigen.com>
2025-04-02 11:00:19 +0530

    minor update

--- db/db.go
+++ db/db.go
@@ -54,20 +54,4 @@ func CreateTable() {
 
 	}
 
-	createRegistrationTable := `
-	 CREATE TABLE IF NOT EXISTS registrations(
-	 id INTEGER PRIMARY KEY AUTOINCREMENT,
-	 event_id INTEGER,
-	 user_id  INTEGER, 
-	 FOREIGN KEY(event_id) REFERENCES events(id),
-	 FOREIGN KEY(user_id) REFERENCES users(id),
-	 )
-	`
-	_, err = DB.Exec(createRegistrationTable)
-
-	if err != nil {
-		log.Fatal("Could not create events table:", err)
-		panic("could not create registraion table")
-
-	}
 }
